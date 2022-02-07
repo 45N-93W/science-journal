023c9344cb79e4d1ec5da8bf4c6e139ced8a7ad3
Atheprogrammer <98733958+Atheprogrammer@users.noreply.github.com>
2022-02-07 09:53:01 -0800

    Add files via upload

---
+++ about/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="journal ">
+    <style type="text/css">
+        header {
+            color: white;
+            background: gray;
+        }
+
+        .nav-menu .nav-list {
+            display: inline-block;
+            padding: 4px;
+        }
+
+        .nav-list .nav-list li a {
+            display: inline-block;
+            list-style-type: none;
+        }
+    </style>
+</head>
+
+<body>
+    <header>about page</header>
+    <nav class="nav-menu">
+        <ul class="nav-list">
+            <li class="active">
+                <a href="\">Home</a>
+            </li>
+            <li>
+                <a href=".\about">About</a>
+            </li>
+            <li>
+                <a href=".\settings">Settings</a>
+            </li>
+
+        </ul>
+    </nav>
+    <main>
+        <div class="page">
+            <h1>about me</h1>
+            <p> i am a programmer called athepony have a another but i would reather not use it.</p>
+            <p>created for n43w92</p>
+
+
+        </div>
+    </main>
+</body>
+
+</html>
