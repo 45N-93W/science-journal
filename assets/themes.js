9f70ee0e5a6d09dd5bff549eb9e2cc78f74d6b6d
technoshy <98733958+Atheprogrammer@users.noreply.github.com>
2022-02-08 11:24:10 -0800

    Add files via upload

---
+++ assets/themes.js
@@ -0,0 +1,24 @@
+function SetLightMode() {
+    var header = document.getElementsByTagName("header");
+    var button = document.getElementsByTagName("button");
+
+    document.body.style.background = "white";
+    document.body.style.color = "black";
+    header.item(1).style.background = "black";
+    button.item(0).style.background = "black";
+    button.item(1).style.background = "black";
+    button.item(0).style.color = "white";
+    button.item(1).style.color = "white";
+
+
+}
+function SetDarkMode() {
+    document.body.style.background = "black";
+    document.body.style.color = "white";
+    header.item(1).style.background = "white";
+    button.item(0).style.background = "white";
+    button.item(1).style.background = "white";
+    button.item(0).style.color = "black";
+    button.item(1).style.color= "black"
+
+}
